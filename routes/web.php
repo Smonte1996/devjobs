14619f3f6710198ac49c22820ae0d6a72fa0982b
Steven Montenegro <smontenegrot@ransa.net>
2023-01-04 17:53:50 -0500

    devjobs

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::get('/', function () {
 
 Route::get('/dashboard', [VacanteController::class, 'index'])->middleware(['auth', 'verified'])->name('vacante.index');
 Route::get('/vacantes/create', [VacanteController::class, 'create'])->middleware(['auth', 'verified'])->name('vacante.create');
+Route::get('/vacantes/{vacante}edit', [VacanteController::class, 'edit'])->middleware(['auth', 'verified'])->name('vacante.edit');
+Route::get('/vacantes/{vacante}', [VacanteController::class, 'show'])->name('vacante.show');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
